18b449d9f44ef84e21be84a62e6a6582613c7d47
zgulen <gulenzubeyir@gmail.com>
2022-06-14 18:17:45 +0300

    not finished yet

---
+++ app.js
@@ -0,0 +1,41 @@
+const url = "https://api.openweathermap.org/data/2.5/"
+const key = "0c782db722756c7a678544f42572f87d"
+
+const button = document.querySelector("button")
+const inputValue = document.querySelector("input")
+const ulCities = document.querySelector(".cities")
+
+button.addEventListener("click", () =>{
+    let link = `${url}weather?q=${inputValue.value}&appid=${key}&units=metric&lang=en`
+    fetch(link)
+    .then(weather =>{
+        return weather.json()
+    })
+    .then(displayResult)
+})
+
+const displayResult = (result) =>{
+    console.log(result)
+    let li = document.createElement("li")
+    let city = document.createElement("p")
+    let temperature = document.createElement("span")
+    let icon = document.createElement("img")
+    let description = document.createElement("figcaption")
+    li.className = "city"
+    city.className = "city-name"
+    temperature.className = "city-temp"
+    icon.className = "city-icon"
+    ulCities.appendChild(li)
+    li.appendChild(city)
+    li.appendChild(temperature)
+    li.appendChild(icon)
+    li.appendChild(description)
+    
+    city.innerHTML = `${result.name}  <sup>${result.sys.country}</sup>`
+    temperature.textContent = `${Math.round(result.main.temp_max)}°C`
+    description.textContent = `${result.weather[0].description}`
+    const iconurl = "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png";
+    icon.src = iconurl
+}
+
+
